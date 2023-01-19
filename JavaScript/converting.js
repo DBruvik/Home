3ef230c48c004edcf310818905e803ae061b41a9
Bruvik <99970850+DBruvik@users.noreply.github.com>
2023-01-19 14:15:37 +0100

    JS work

---
+++ JavaScript/converting.js
@@ -0,0 +1,11 @@
+let nrToStr = 6;
+nrToStr = String(nrToStr);
+console.log(nrToStr, typeof nrToStr); //String
+
+let strToNr ="12";
+strToNr = Number(strToNr);
+console.log(strToNr, typeof strToNr);
+
+let strToBool = "Any string will return true";
+strToBool = Boolean(strToBool);
+console.log(strToBool, typeof strToBool);
---
+++ JavaScript/tasksP32.js
@@ -0,0 +1,32 @@
+let str1 = 'Laurence';
+let str2 = "Svekis";
+let val1 = undefined;
+let val2 = null;
+let myNum = 1000;
+
+print1 = (typeof str1);
+print2 = (typeof str2);
+print3 = (typeof val1);
+print4 = (typeof val2);
+print5 = (typeof myNum);
+
+console.log(print1);
+console.log(print2);
+console.log(print3);
+console.log(print4);
+console.log(print5);
+
+
+//Tasks P33
+let name = "Daniel";
+let age = 29;
+let code = "JavaScript";
+
+console.log(`My name is ${name}, I am ${age} years old and I love ${code}.`);
+
+
+//Tasks P37
+let nr1 = 4;
+let nr2 = 5;
+let nr3 = 2;
+console.log(nr1++ + ++ nr2 * nr3++);
